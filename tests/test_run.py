d266e7571d9c894fc452126f77ad911284f07bf6
Doug Latornell <dlatornell@eoas.ubc.ca>
2021-07-20 11:24:33 -0700

    Add tests for `atlantis run` command-line parser

--- tests/test_run.py
+++ tests/test_run.py
@@ -14,6 +14,8 @@
 # limitations under the License.
 """AtlantisCmd run sub-command plug-in unit and integration tests.
 """
+from pathlib import Path
+
 import pytest
 
 import atlantis_cmd.main
@@ -32,3 +34,58 @@ class TestParser:
     def test_get_parser(self, run_cmd):
         parser = run_cmd.get_parser("atlantis run")
         assert parser.prog == "atlantis run"
+
+    def test_cmd_description(self, run_cmd):
+        parser = run_cmd.get_parser("atlantis run")
+        assert parser.description.strip().startswith(
+            "Prepare, execute, and gather the results from an Atlantis"
+        )
+
+    def test_desc_file_argument(self, run_cmd):
+        parser = run_cmd.get_parser("atlatnis run")
+        assert parser._actions[1].dest == "desc_file"
+        assert parser._actions[1].metavar == "DESC_FILE"
+        assert parser._actions[1].type == Path
+        assert parser._actions[1].help
+
+    def test_results_dir_argument(self, run_cmd):
+        parser = run_cmd.get_parser("atlantis run")
+        assert parser._actions[2].dest == "results_dir"
+        assert parser._actions[2].metavar == "RESULTS_DIR"
+        assert parser._actions[2].type == Path
+        assert parser._actions[2].help
+
+    def test_no_submit_option(self, run_cmd):
+        parser = run_cmd.get_parser("atlantis run")
+        assert parser._actions[3].dest == "no_submit"
+        assert parser._actions[3].option_strings == ["--no-submit"]
+        assert parser._actions[3].const is True
+        assert parser._actions[3].default is False
+        assert parser._actions[3].help
+
+    def test_quiet_option(self, run_cmd):
+        parser = run_cmd.get_parser("atlantis run")
+        assert parser._actions[4].dest == "quiet"
+        assert parser._actions[4].option_strings == ["-q", "--quiet"]
+        assert parser._actions[4].const is True
+        assert parser._actions[4].default is False
+        assert parser._actions[4].help
+
+    def test_parsed_args_defaults(self, run_cmd):
+        parser = run_cmd.get_parser("atlantis run")
+        parsed_args = parser.parse_args(["foo.yaml", "results/foo/"])
+        assert parsed_args.desc_file == Path("foo.yaml")
+        assert parsed_args.results_dir == Path("results/foo/")
+        assert not parsed_args.no_submit
+        assert not parsed_args.quiet
+
+    @pytest.mark.parametrize("flag", ["-q", "--quiet"])
+    def test_parsed_args_quiet_options(self, flag, run_cmd):
+        parser = run_cmd.get_parser("atlantis run")
+        parsed_args = parser.parse_args(["foo.yaml", "results/foo/", flag])
+        assert parsed_args.quiet is True
+
+    def test_parsed_args_no_submit_option(self, run_cmd):
+        parser = run_cmd.get_parser("atlantis run")
+        parsed_args = parser.parse_args(["foo.yaml", "results/foo/", "--no-submit"])
+        assert parsed_args.no_submit is True
