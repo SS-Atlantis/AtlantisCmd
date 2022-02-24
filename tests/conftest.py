bdd81ab9d39dfeaa2b2eb2d6296206a411c1f62c
Doug Latornell <dlatornell@eoas.ubc.ca>
2022-02-24 14:49:15 -0800

    Add SPDX short-form license identifiers
    Communicates license information in portable and machine-readable manner.

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#
+
+# SPDX-License-Identifier: Apache-2.0
+
+
 """Shared fixtures for AtlantisCmd unit and integration tests.
 """
 import os
